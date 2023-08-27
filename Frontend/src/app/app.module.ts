14b4fd19dacfb1b2cbffa72a66566343650971fe
ArtemFlorko <artom.florko@gmail.com>
2023-08-27 23:31:50 +0300

    Enhanced user input validation, added password visibility toggle, and tooltips for inputs at Sign Up/Login page

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTooltipModule } from "@angular/material/tooltip"
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, AuthComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTooltipModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- Frontend/src/app/auth/auth.component.ts
+++ Frontend/src/app/auth/auth.component.ts
@@ -1,8 +1,46 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {}
+
+export class AuthComponent {
+  loginForm: FormGroup;
+  signupForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.pattern('^[A-Z][a-z]{1,}$')]],
+      lastName: ['', [Validators.required, Validators.pattern('^[A-Z][a-z]{1,}$')]],
+      signupEmail: ['', [Validators.required, Validators.pattern('^[A-Za-z0-9\.]+@[a-z]+\.[a-z]{2,3}$')]],
+      signupPassword: ['', [Validators.required, Validators.minLength(8)]],
+    });
+
+    this.loginForm = this.fb.group({
+      loginEmail: ['', [Validators.required, Validators.pattern('^[A-Za-z0-9\.]+@[a-z]+\.[a-z]{2,3}$')]],
+      loginPassword: ['', [Validators.required, Validators.pattern('^[A-Za-z0-9!@#$%]{8,20}$')]],
+    });
+  }
+
+  switchToSignup(): void {
+    this.loginForm.reset();
+  }
+
+  switchToLogin(): void {
+    this.signupForm.reset();
+  }
+
+  showLoginPassword: boolean = false;
+  showSignupPassword: boolean = false;
+
+  toggleLoginPassword(): void {
+    this.showLoginPassword = !this.showLoginPassword;
+  }
+
+  toggleSignupPassword(): void {
+    this.showSignupPassword = !this.showSignupPassword;
+  }
+}
