cd53f8c88dcdce1bb79b6855c090e95dc33f05b2
ArtemFlorko <artom.florko@gmail.com>
2023-10-06 15:38:55 +0300

    Main Page: added saving of created tasks. So far, only on the frontend.

--- Frontend/src/app/task-list/task-list.component.ts
+++ Frontend/src/app/task-list/task-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../models/task.model';
+import { TaskService } from '../services/task.service';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
+  }
 }
