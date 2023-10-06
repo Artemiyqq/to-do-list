654fe5f26bf16c983c9d38963e680ce586591783
ArtemFlorko <artom.florko@gmail.com>
2023-10-06 15:37:23 +0300

    Task Creation Modal: disabled to select past date in Due Date input.

--- Frontend/src/app/task-create-modal/task-create-modal.component.ts
+++ Frontend/src/app/task-create-modal/task-create-modal.component.ts
@@ -24,13 +24,11 @@ export class TaskCreateModalComponent {
     this.closeTaskModal();
   }
 
-  getMinDateTime(): string {
+  getMinDate(): string {
     const today = new Date();
     const year = today.getFullYear();
     const month = (today.getMonth() + 1).toString().padStart(2, '0');
     const day = today.getDate().toString().padStart(2, '0');
-    const hours = today.getHours().toString().padStart(2, '0');
-    const minutes = today.getMinutes().toString().padStart(2, '0');
-    return `${year}-${month}-${day}T${hours}:${minutes}`;
+    return `${year}-${month}-${day}`;
   }
 }
