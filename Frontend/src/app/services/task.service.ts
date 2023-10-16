31e5b00e524e9e4498b301f273664fef0ddcad7d
ArtemFlorko <artom.florko@gmail.com>
2023-10-16 13:29:08 +0300

    Frontend services: implemented POST request of a task in the task service. Implemented getting the API URL in the user service from the config service.

--- Frontend/src/app/services/task.service.ts
+++ Frontend/src/app/services/task.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Task } from '../models/task.model';
 import { UserService } from './user.service';
+import { ConfigService } from './config.service';
+import { HttpClient } from '@angular/common/http';
+import { TaskDto } from '../models/task-dto.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,27 +11,41 @@ import { UserService } from './user.service';
 export class TaskService {
   private tasks: Task[] = [];
 
-  constructor(private userService: UserService) { }
+  constructor(private http: HttpClient, private configService: ConfigService, private userService: UserService) { }
 
-  createTask(title: string, description: string, dueDate: string): void {
-    const task = new Task();
-    task.title = title;
-    task.description = description;
-    task.dueDate = new Date(dueDate);
-    
+  processNewTask(title: string, description: string, dueDate: string) {
+    const newTaskDto = this.createTaskDtoObject(title, description, dueDate);
+    if (newTaskDto != null){
+      this.postTask(newTaskDto).subscribe({
+        error: (e) => console.error("Error saving task on backend:", e),
+        next: (response) => {
+          const newTask = new Task(response.id, title, description, false, new Date(newTaskDto.dueDate), newTaskDto.userId);
+          this.tasks.push(newTask)}
+      })
+    }
+    else {
+      console.error("There is a problem with processing a new task")
+    }
+  }
+
+  createTaskDtoObject(title: string, description: string, dueDate: string): TaskDto | null {
     const userId = this.userService.getUserId();
     if (userId != null)
     {
-      task.userId = userId;
+      const taskDto = new TaskDto(title, description, dueDate, userId);
+      return taskDto;
     }
     else {
-      console.error("There is a problem with the user id. Cannot save new task.")
+      console.error("There is a problem with the user id.")
+      return null;
     }
-
-    this.tasks.push(task);
   }
 
   getTasks(): Task[] {
     return this.tasks;
+  }
+
+  postTask(task: TaskDto) {
+    return this.http.post<{id: number}>(`${this.configService.getApiBaseUrl()}/api/tasks`, task);
   }
 }
--- Frontend/src/app/services/user.service.ts
+++ Frontend/src/app/services/user.service.ts
@@ -1,15 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ConfigService } from './config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiBaseurl = '';
   private userId: number | null = null;
 
-  constructor(private http: HttpClient) { }
+  constructor(private configService: ConfigService, private http: HttpClient) { }
 
   setUserId(userId: number) {
     this.userId = userId;
@@ -24,6 +24,6 @@ export class UserService {
   }
 
   getUserFullName(): Observable<any> {
-    return this.http.get(`${this.apiBaseurl}/api/users/get-full-name?userId=${this.getUserId()}`);
+    return this.http.get(`${this.configService.getApiBaseUrl()}/api/users/get-full-name?userId=${this.getUserId()}`);
   }
 }
